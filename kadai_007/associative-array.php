a37092c51c0bb70a552b66545fb719866b6f65a4
ypb-sd12 <takahiro.hado@yoshida-p.co.jp>
2025-04-21 13:17:52 +0900

    kadai_007を作成

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $items = ["name" => "onion", "price" => 200, "weight" => 160];
+        print_r($items);
+        ?>
+    </p>
+</body>
+
+</html>
